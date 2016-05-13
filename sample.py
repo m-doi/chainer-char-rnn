dcb367c6aac6680eb395d261c574f0971b1e8f29
土井 正宏 <m-doi@biglobe.co.jp>
2016-05-14 05:40:13 +0900

    Chainerの最新バージョンに対応

--- sample.py
+++ sample.py
@@ -4,14 +4,14 @@ import math
 import sys
 import argparse
 import cPickle as pickle
-import codecs
 
 import numpy as np
 from chainer import cuda, Variable, FunctionSet
 import chainer.functions as F
 from CharRNN import CharRNN, make_initial_state
+import codecs
 
-sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
+import MeCab
 
 #%% arguments
 parser = argparse.ArgumentParser()
@@ -29,6 +29,8 @@ args = parser.parse_args()
 
 np.random.seed(args.seed)
 
+mt = MeCab.Tagger('-Ochasen')
+
 # load vocabulary
 vocab = pickle.load(open(args.vocabulary, 'rb'))
 ivocab = {}
@@ -40,7 +42,7 @@ model = pickle.load(open(args.model, 'rb'))
 n_units = model.embed.W.data.shape[1]
 
 if args.gpu >= 0:
-    cuda.get_device(args.gpu).use()
+    cuda.init()
     model.to_gpu()
 
 # initialize generator
@@ -49,21 +51,42 @@ if args.gpu >= 0:
     for key, value in state.items():
         value.data = cuda.to_gpu(value.data)
 
-prev_char = np.array([0], dtype=np.int32)
+prev_char = np.array([0])
 if args.gpu >= 0:
     prev_char = cuda.to_gpu(prev_char)
 
+sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
+
 if len(args.primetext) > 0:
-    for i in unicode(args.primetext, 'utf-8'):
-        sys.stdout.write(i)
-        prev_char = np.ones((1,), dtype=np.int32) * vocab[i]
+    words = []
+    result = mt.parseToNode(args.primetext)
+    while result:
+        words.append(unicode(result.surface, 'utf-8'))
+        result = result.next
+    
+    for word in words:
+        if (word in vocab):
+            val = vocab[word]
+        else:
+            val = 0
+        prev_char = np.ones((1,)).astype(np.int32) * val
         if args.gpu >= 0:
             prev_char = cuda.to_gpu(prev_char)
 
-        state, prob = model.forward_one_step(prev_char, prev_char, state, train=False)
+        state, prob = model.predict(prev_char, state)
+
+    # for i in unicode(args.primetext, 'utf-8'):
+    #     print i
+    #     sys.stdout.write(i)
+    #     print vocab[i]
+    #     prev_char = np.ones((1,)).astype(np.int32) * vocab[i]
+    #     if args.gpu >= 0:
+    #         prev_char = cuda.to_gpu(prev_char)
+    #
+    #     state, prob = model.predict(prev_char, state)
 
 for i in xrange(args.length):
-    state, prob = model.forward_one_step(prev_char, prev_char, state, train=False)
+    state, prob = model.predict(prev_char, state)
 
     if args.sample > 0:
         probability = cuda.to_cpu(prob.data)[0].astype(np.float64)
@@ -73,7 +96,7 @@ for i in xrange(args.length):
         index = np.argmax(cuda.to_cpu(prob.data))
     sys.stdout.write(ivocab[index])
 
-    prev_char = np.array([index], dtype=np.int32)
+    prev_char = np.array([index])
     if args.gpu >= 0:
         prev_char = cuda.to_gpu(prev_char)
 
